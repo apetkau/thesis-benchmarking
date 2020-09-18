8860fee10f86b55836b8f76152e87da615bc8b43
Aaron Petkau <aaron.petkau@canada.ca>
2020-09-18 10:19:35 -0500

    Fixed up BIGSI benchmarking

---
+++ lib/benchmark.py
@@ -0,0 +1,27 @@
+import imp
+import os
+import shutil
+
+cmdbench_dir = os.path.dirname(os.path.abspath(__file__)) + '/cmdbench/bioinformatics'
+
+fp, pathname, description = imp.find_module('multibench', [cmdbench_dir])
+multibench = imp.load_module('multibench', fp, pathname, description)
+
+def clean_if_exists(path):
+    if os.path.exists(path):
+        if(os.path.isfile(path)):
+            os.remove(path)
+        else:
+            shutil.rmtree(path)
+            os.mkdir(path)
+            
+def get_last_n_lines(string, n):
+    return "\n".join(string.split("\n")[-n:])
+    
+def benchmark_list_to_results(benchmark_firsts_list):
+    return {
+        "memory": max(list(map(lambda result: result.memory.max, benchmark_firsts_list))),
+        "disk_read": max(list(map(lambda result: result.disk.read_chars, benchmark_firsts_list))),
+        "disk_write": max(list(map(lambda result: result.disk.write_chars, benchmark_firsts_list))),
+        "runtime": sum(list(map(lambda result: result.process.execution_time, benchmark_firsts_list)))
+    }
